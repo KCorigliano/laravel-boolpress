00e5d6587a17fdc73c5ff0968b9cd05b5fd81b14
KCorigliano <corigliano98@hotmail.com>
2022-03-11 15:17:30 +0100

    add new route admin home

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -4,8 +4,29 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
-class HomeController extends Controller
-{
-    //
+class HomeController extends Controller {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct() {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index() {
+        $user = Auth::user();
+
+
+        // dd($user);
+
+        return view('admin.home');
+    }
 }
