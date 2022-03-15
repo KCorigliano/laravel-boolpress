627fdaaa58d300e2fdf0673c3eefae49ac75958a
KCorigliano <corigliano98@hotmail.com>
2022-03-15 16:19:53 +0100

    add categories table

---
+++ database/migrations/2022_03_15_151611_add_foreign_categories_key_in_post_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddForeignCategoriesKeyInPostTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('post', function (Blueprint $table) {
+            $table->foreignId("category_id")
+                ->nullable()
+                ->references("user_id")
+                ->on("users");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('post', function (Blueprint $table) {
+            //
+        });
+    }
+}
